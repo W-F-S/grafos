4738026f5984eb6b649c5f8dc0102f6e1cebba92
WFS <walkerf082@gmail.com>
2022-03-20 22:33:48 -0300

    avanco

---
+++ grafo.c
@@ -0,0 +1,36 @@
+#include "linked_list.h"
+//criando um grafo direcional não ponderado
+vertice *grafo;
+
+
+
+/*
+ * n = número de vertices
+ * nodes = vertices e arestas
+ */
+
+vertice *create_graph(int nodes[], int n){
+
+}
+
+
+int main(){
+  int *nodo = calloc(8, sizeof(int));
+  int n_nodo = sizeof(nodo)/sizeof(int*);
+  nodo[0]
+  nodo[1]  
+  nodo[2]
+  nodo[3]
+
+  while(1){
+    
+
+  }
+}
+
+void new_vertice(vertice *grafo, int valor){
+    vertice *temp = malloc(sizeof(vertice));
+    temp->valor_vertice = valor;
+    
+}
+
